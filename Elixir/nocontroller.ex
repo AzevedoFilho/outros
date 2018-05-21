e41e7c818bc4f5beac2f36098ac4b429d3bd3de0
AzevedoFilho <azevedofilho.oracle@outlook.com>
2018-05-21 19:14:07 -0300

    outros tipos

---
+++ Elixir/nocontroller.ex
@@ -0,0 +1,8 @@
+defmodule MyApp.MyController do
+  use MyApp.Web, :controller
+  use Guardian.Phoenix.Controller
+
+  def some_action(conn, params, user, claims) do
+    # do stuff
+  end
+end
---
+++ Elixir/scope.ex
@@ -0,0 +1,13 @@
+scope "/", MyApp do
+  pipe_through([:browser, :maybe_browser_auth])
+
+  get("/login", LoginController, :new)
+  post("/login", LoginController, :create)
+  delete("/login", LoginController, :delete)
+end
+
+scope "/", MyApp do
+  pipe_through([:browser, :maybe_browser_auth, :ensure_authed_access])
+
+  resource("/protected/things", ProtectedController)
+end
